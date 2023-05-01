c40164ec93d1e5c9b735164db516f6bed939e37e
rkhan570 <ruba.khan2014@gmail.com>
2023-05-01 18:18:02 -0400

    wowowow

--- app.py
+++ app.py
@@ -1,26 +1,68 @@
+import openai
+from flask import Flask, request, render_template
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper, \
+    ServiceContext
+from langchain.chat_models import ChatOpenAI
+import sys
 import os
 
-from flask import Flask, render_template, request
-import openai
-
 app = Flask(__name__)
+API_KEY = os.environ.get("OPENAI_API_KEY")
+openai.api_key = "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"
 
-#openai tools fine_tunes.prepare_data -f .\chatbotEnvironment\chatbotTrain.jsonl
-#openai --api-key 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 api fine_tunes.create -t .\chatbotEnvironment\chatbotTrain_prepared.jsonl -m ada
-#openai --api-key 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 api fine_tunes.list
-@app.route("/", methods = ["GET", "POST"])
+
+def construct_index(directory_path):
+    # set maximum input size
+    max_input_size = 4096
+    # set number of output tokens
+    num_outputs = 2000
+    # set maximum chunk overlap
+    max_chunk_overlap = 20
+    # set chunk size limit
+    chunk_size_limit = 600
+
+    # define prompt helper
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    # define LLM
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    print(directory_path)
+    print(documents)
+
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+
+    index.save_to_disk('index.json')
+
+    return index
+
+# Define chatbot function
+def ask_ai(prompt):
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    print(f"Prompt: {prompt}")
+    response = index.query(prompt)
+    print(f"Response: {response}")
+    if response is None:
+        response = "I'm sorry, I do not understand"
+    return response
+
+
+# Define Flask routes
+@app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        prompt = request.form["prompt"]
-        response = openai.Completion.create(
-            model = "ada:ft-personal-2023-05-01-09-12-33",
-            prompt = prompt,
-            n = 1,
-            max_tokens = 1024,
-            stop = None,
-        )
-        return render_template("home.html", response = response.choices[0])
-    return render_template("home.html")
+        prompt = request.form.get('prompt', '').strip()
+        if prompt:
+            response = ask_ai(prompt)
+        else:
+            response = "Please enter a prompt"
+        return render_template('home.html', response=response)
+    return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run(debug = True)
+
+# Start Flask application
+if __name__ == '__main__':
+    construct_index("chatbotEnvironment/")
+    app.run(debug=True)
